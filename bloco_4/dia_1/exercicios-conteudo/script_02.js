812314f9d14a85020458a6aeedd240afff39abf0
Jessica Arruda <sicarruda@gmail.com>
2021-03-24 15:53:43 -0300

    exercicos do conteudo + exercicios Javascript

---
+++ bloco_4/dia_1/exercicios-conteudo/script_02.js
@@ -0,0 +1,28 @@
+let nota = 100;
+
+if (nota > 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} else if (nota > 80 && nota < 60) {
+  console.log("Você está na nossa lista de espera");
+} else {
+  console.log("Você foi reprovada(o)");
+}
+
+//switch -- case
+
+let semaforo;
+
+switch (semaforo) {
+  case "vermelho":
+    console.log("pare");
+    break;
+	case "amarelo":
+		console.log("atenção");
+		break
+	case "verde":
+		console.log("siga");
+		break;
+
+	default:
+		console.log("valor não identificado");
+}
