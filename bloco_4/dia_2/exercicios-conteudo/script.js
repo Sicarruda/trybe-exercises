27b29bd6c1124cce8f03dfd203953a176918da40
Jessica Arruda <sicarruda@gmail.com>
2021-03-25 18:24:11 -0300

    Exercicos do bloco 4

---
+++ bloco_4/dia_2/exercicios-conteudo/script.js
@@ -0,0 +1,9 @@
+let names = ["João", "Maria", "Antônio", "Margarida"];
+for (let nome of names) {
+  console.log(nome);
+}
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+for (let numero of numeros) {
+  numero -= 1;
+  console.log(numero)
+}
