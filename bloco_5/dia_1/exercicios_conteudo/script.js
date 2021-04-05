3ff8d7abeb1e39d91e041065440e158360511e70
Jessica Arruda <sicarruda@gmail.com>
2021-04-05 19:01:03 -0300

    Exercicios do conteudo do bloco 5

---
+++ bloco_5/dia_1/exercicios_conteudo/script.js
@@ -0,0 +1,14 @@
+var divRed = document.querySelector("div");
+divRed.style.backgroundColor = "red";
+
+var divBlue = document.getElementsByClassName("emergency-tasks")[0];
+divBlue.style.backgroundColor = "blue";
+
+var h3black = document.querySelectorAll(".no-emergency-tasks h3")[1];
+h3black.style.backgroundColor = "black";
+
+var h3Yellow = document.getElementsByTagName("h3")[0]
+h3Yellow.style.backgroundColor = "yellow"
+
+var h1White = document.getElementById("header-container")
+h1White.style.backgroundColor = "white"
